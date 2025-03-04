d0ac8e12b19017307b1cdcb6998ffe83ad9dac42
Paavo Penttila <thxmuffe@hotmail.com>
2025-03-04 14:39:50 +0200

    Add code which build web app, and starts it with controllers

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 class Program {
     public static void Main(string[] args) {
+        Console.WriteLine("Ohjelma aloittaa");
+
+        // b niinkuin Builder
+        var b = WebApplication.CreateBuilder();
+
+        b.Services.AddControllers();
+
+        // w niinkuij WebApp
+        var w = b.Build(); // Luo valmiin web application, joka seuraavaksi voidaan käynnistää
+
+        w.MapControllers();
         
+        w.Run();
 
 
-
-        
+        Console.WriteLine("Ohjelma lopettaa");
     } 
 }
